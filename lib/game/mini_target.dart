692cf49d3cb0153062a88999469647722b644f06
Daniel Lukic <daniel.lukic@gmx.net>
2024-06-18 16:15:46 +0200

    Add collision damage to MiniTarget.applyDamage

--- lib/game/mini_target.dart
+++ lib/game/mini_target.dart
@@ -11,8 +11,8 @@ mixin MiniTarget on Component3D {
   double life = 3;
 
   /// returns true when destroyed
-  bool applyDamage({double? plasma, double? laser, double? missile}) {
-    life -= (plasma ?? 0) + (laser ?? 0) + (missile ?? 0);
+  bool applyDamage({double? collision, double? plasma, double? laser, double? missile}) {
+    life -= (collision ?? 0) + (plasma ?? 0) + (laser ?? 0) + (missile ?? 0);
     if (life <= 0) {
       spawnEffect(MiniEffectKind.explosion, this);
       removeFromParent();
