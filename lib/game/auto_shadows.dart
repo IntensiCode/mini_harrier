7acc8dfe4fae16fa1070f2423c5d00fec23f5cf3
Daniel Lukic <daniel.lukic@gmx.net>
2024-06-18 19:28:36 +0200

    Make shadows vanish more eagerly

--- lib/game/auto_shadows.dart
+++ lib/game/auto_shadows.dart
@@ -4,8 +4,10 @@ import 'package:collection/collection.dart';
 import 'package:flame/components.dart';
 
 import '../core/mini_3d.dart';
+import '../core/mini_common.dart';
 import '../scripting/mini_script.dart';
 import '../util/extensions.dart';
+import 'mini_effects.dart';
 
 class AutoShadows extends MiniScriptComponent {
   final shadows = <Component3D, Shadow>{};
@@ -14,16 +16,23 @@ class AutoShadows extends MiniScriptComponent {
   void update(double dt) {
     super.update(dt);
 
-    final active = parent?.children.whereType<Component3D>().whereNot((it) => it is Shadow).toList();
+    final active = parent?.children
+        .whereType<Component3D>() //
+        .whereNot((it) => it is Shadow) //
+        .whereNot((it) => it is MiniEffect)
+        .toList();
+
     if (active == null) return;
 
     final gone = shadows.keys.where((it) => !active.contains(it)).toList();
+    for (final it in shadows.entries) {
+      if (it.value.removeMe) gone.add(it.key);
+    }
     for (final child in gone) {
       final shadow = shadows.remove(child);
-      if (shadow != null) {
-        shadow.removeFromParent();
-        pool.add(shadow);
-      }
+      if (shadow == null) continue;
+      shadow.removeFromParent();
+      pool.add(shadow);
     }
 
     final add = active.where((it) => !shadows.containsKey(it)).toList();
@@ -38,24 +47,33 @@ class AutoShadows extends MiniScriptComponent {
   final pool = <Shadow>[];
 }
 
-class Shadow extends Component3D {
+class Shadow extends Component3D with HasVisibility {
   Shadow({required super.world}) {
-    add(CircleComponent(radius: 20, anchor: Anchor.center, paint: Paint()..color = const Color(0x80000000))
-      ..scale.setValues(5, 2));
+    it = added(CircleComponent(radius: 20, anchor: Anchor.center, paint: Paint()..color = const Color(0x80000000)));
+    it.scale.setValues(5, 2);
   }
+
+  late final CircleComponent it;
 
   late Component3D _source;
 
-  set source(Component3D value) {
-    _source = value;
-    scale.setFrom(value.size);
-  }
+  set source(Component3D value) => _source = value;
 
   @override
   void update(double dt) {
     super.update(dt);
-    worldPosition.x = _source.worldPosition.x;
+
+    worldPosition.x = _source.worldPosition.x - _source.worldPosition.y / 2;
     worldPosition.y = 0;
     worldPosition.z = _source.worldPosition.z;
+
+    if (position.x < -50) removeMe = true;
+    if (position.x > gameWidth + 50) removeMe = true;
+    if (position.y > gameHeight + 50) removeMe = true;
+    if (_source.worldPosition.z < world.camera.z - 2000) removeMe = true;
+    if (_source.worldPosition.z > world.camera.z) removeMe = true;
+    if (_source.isMounted == false) removeMe = true;
   }
+
+  bool removeMe = false;
 }
