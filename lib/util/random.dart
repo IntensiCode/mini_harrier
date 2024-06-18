7ed3ae019bee89768c913ee31380d77ce4fabdcf
Daniel Lukic <daniel.lukic@gmx.net>
2024-06-18 16:13:18 +0200

    Add randomNormalizedVector3 (and fix for Vector2)

--- lib/util/random.dart
+++ lib/util/random.dart
@@ -4,4 +4,6 @@ import 'package:flame/components.dart';
 
 final random = Random();
 
-Vector2 randomNormalizedVector() => Vector2.random(random) - Vector2.random();
+Vector2 randomNormalizedVector2() => Vector2.random(random) - Vector2.random(random);
+
+Vector3 randomNormalizedVector3() => Vector3.random(random) - Vector3.random(random);
