9f2bb0bad9e19ae2fea4964b0b011b1e9fa5f765
Daniel Lukic <daniel.lukic@gmx.net>
2024-06-19 00:26:01 +0200

    Update unused kamikaze ufo enemy

---
+++ lib/game/kamikaze_ufo_enemies.dart
@@ -0,0 +1,45 @@
+import '../core/mini_3d.dart';
+import '../core/mini_messaging.dart';
+import '../scripting/mini_script.dart';
+import 'kamikaze_ufo_enemy.dart';
+import 'ufo_enemy.dart';
+
+class KamikazeUfoEnemies extends MiniScriptComponent {
+  KamikazeUfoEnemies(this.captain, [this.waveSize = 10, this.spawnInterval = 2.0]);
+
+  final Component3D captain;
+  final int waveSize;
+  final double spawnInterval;
+
+  late var remainingEnemies = waveSize;
+  late var nextSpawnTime = spawnInterval;
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (captain.isMounted == false) {
+      parent?.children.whereType<UfoEnemy>().forEach((it) => it.flyOff());
+      removeFromParent();
+      return;
+    }
+    if (remainingEnemies == 0) {
+      return;
+    } else if (nextSpawnTime <= 0) {
+      parent!.add(KamikazeUfoEnemy(_onDefeated, world: world));
+      nextSpawnTime = spawnInterval;
+      remainingEnemies--;
+    } else {
+      nextSpawnTime -= dt;
+    }
+  }
+
+  var defeatedEnemies = 0;
+
+  void _onDefeated() {
+    defeatedEnemies++;
+    if (defeatedEnemies == waveSize) {
+      sendMessage(EnemiesDefeated());
+      removeFromParent();
+    }
+  }
+}
--- lib/game/kamikaze_ufo_enemy.dart
+++ lib/game/kamikaze_ufo_enemy.dart
@@ -4,33 +4,44 @@ import 'package:dart_minilog/dart_minilog.dart';
 import 'package:flame/components.dart';
 
 import '../core/mini_3d.dart';
+import '../core/mini_common.dart';
 import '../scripting/mini_script.dart';
 import '../scripting/mini_script_functions.dart';
 import '../util/auto_dispose.dart';
 import '../util/extensions.dart';
 import '../util/random.dart';
+import 'fragment.dart';
 import 'mini_target.dart';
 
 enum _State {
   incoming,
-  floating,
+  attacking,
+  fly_off,
 }
 
 class KamikazeUfoEnemy extends Component3D with AutoDispose, MiniScriptFunctions, MiniScript, MiniTarget {
-  KamikazeUfoEnemy(this.onDefeated, {required super.world});
+  KamikazeUfoEnemy(this.onDefeated, {required super.world}) {
+    anchor = Anchor.bottomCenter;
+  }
 
   final void Function() onDefeated;
 
   _State _state = _State.incoming;
 
+  bool readyToAttack = false;
+
+  flyOff() => _state = _State.fly_off;
+
+  late final SpriteComponent _sprite;
+
   @override
   onLoad() async {
-    final it = added(await spriteXY('alien-ufo-front.png', 0, 0));
-    it.scale.setAll(3.0);
+    _sprite = added(await spriteXY('alien-ufo-front.png', 0, 0, Anchor.bottomCenter));
+    _sprite.scale.setAll(3.0);
     worldPosition.setFrom(world.camera);
-    worldPosition.z -= 1000;
+    worldPosition.z -= 5000;
     stateTime = random.nextDoubleLimit(4.0);
-    life = 5;
+    life = 10;
   }
 
   var stateTime = 0.0;
@@ -39,62 +50,109 @@ class KamikazeUfoEnemy extends Component3D with AutoDispose, MiniScriptFunctions
 
   final targetVelocity = Vector3(0, 0, 0);
   final velocity = Vector3(0, 0, 0);
-  final relativePosition = Vector3(0, 0, 0);
+  final relativePosition = Vector3(0, 0, -5000);
+  var incomingSpeed = 2500.0;
 
   @override
   void update(double dt) {
     super.update(dt);
     if (_state == _State.incoming) {
-      worldPosition.x = sin(stateTime) * 100;
-      worldPosition.y = 100 + sin(stateTime * 1.4) * cos(stateTime * 1.8) * 75;
-      worldPosition.z -= 300 * dt;
-      stateTime += dt;
-      if (worldPosition.z >= world.camera.z - targetOffsetZ) {
-        _state = _State.floating;
-        velocity.x = sin(stateTime) * 100;
-        velocity.y = 100 + sin(stateTime * 1.4) * cos(stateTime * 1.8) * 75;
-        velocity.z = worldPosition.z + 200 * dt;
-        velocity.sub(worldPosition);
-        relativePosition.setFrom(worldPosition);
-        relativePosition.sub(world.camera);
-        relativePosition.z += targetOffsetZ;
-        targetVelocity.setFrom(velocity);
-        logInfo(relativePosition);
-        logInfo(targetVelocity);
-        // targetVelocity.y = 0;
-        // targetVelocity.z = 0;
-        // velocity.y = 0;
-        // velocity.z = 0;
-      }
-    }
-    if (_state == _State.floating) {
       worldPosition.setFrom(world.camera);
       worldPosition.add(relativePosition);
       worldPosition.z -= targetOffsetZ;
 
-      final xSign = relativePosition.x.sign;
-      // final ySign = relativePosition.y.sign;
-      // final zSign = relativePosition.z.sign;
+      relativePosition.x = sin(stateTime) * 100;
+      relativePosition.y = 100 + sin(stateTime * 1.4) * cos(stateTime * 1.8) * 75;
+      relativePosition.z += incomingSpeed * dt;
+      incomingSpeed = relativePosition.z.abs().clamp(300, 2500);
+
+      stateTime += dt;
+
+      if (worldPosition.z >= world.camera.z - targetOffsetZ) {
+        _state = _State.attacking;
+        velocity.x = sin(stateTime) * 100;
+        velocity.y = 100 + sin(stateTime * 1.4) * cos(stateTime * 1.8) * 75;
+        velocity.z = 250 * dt;
+        velocity.sub(relativePosition);
+        velocity.z = -velocity.z;
+        targetVelocity.setFrom(velocity);
+        logInfo(relativePosition);
+        logInfo(targetVelocity);
+
+        readyToAttack = true;
+      }
+    }
+    if (_state == _State.fly_off) {
+      targetVelocity.setValues(0, 20, 0);
+
+      worldPosition.setFrom(world.camera);
+      worldPosition.add(relativePosition);
+      worldPosition.z -= targetOffsetZ;
+
+      readyToAttack = false;
+
+      relativePosition.add(velocity);
+      relativePosition.z -= 20 * dt;
+
+      velocity.lerp(targetVelocity, 0.01);
+
+      if (relativePosition.y > 10000) {
+        _removeNow();
+        return;
+      }
+      if (position.y < -50) {
+        _removeNow();
+        return;
+      }
+    }
+    if (_state == _State.attacking) {
+      worldPosition.setFrom(world.camera);
+      worldPosition.add(relativePosition);
+      worldPosition.z -= targetOffsetZ;
 
       relativePosition.add(velocity);
 
-      // if (xSign != relativePosition.x.sign) {
-      //   targetVelocity.x = xSign * (10 + random.nextDoubleLimit(10));
-      // }
-      // // if (ySign != relativePosition.y.sign) {
-      // //   targetVelocity.y = -ySign * random.nextDoubleLimit(5);
-      // // }
-      // // if (zSign != relativePosition.z.sign) {
-      // //   targetVelocity.z = -zSign * random.nextDoubleLimit(5);
-      // // }
-      //
-      // velocity.lerp(targetVelocity, 0.3);
+      bool remove = false;
+      if (position.x < -20 || position.x > gameWidth + 20) remove = true;
+      if (position.y > gameHeight + 20) remove = true;
+      if (worldPosition.z > world.camera.z - 20) remove = true;
+      if (remove) {
+        _removeNow();
+        return;
+      }
     }
   }
+
+  void _removeNow() {
+    removeFromParent();
+    onDefeated();
+  }
+
+  final check = Vector3.zero();
 
   @override
   void whenDefeated() {
     onDefeated();
+
+    final i = _sprite.sprite?.image;
+    if (i == null) return;
+
+    final pieces = sheet(i, 5, 5);
+    for (var i = 0; i < 5; i++) {
+      for (var j = 0; j < 5; j++) {
+        final dx = (i - 2) * 10.0;
+        final dy = (j - 2) * 10.0;
+        velocity.setValues(0, 0, -5);
+        parent?.add(Fragment(
+          worldPosition,
+          velocity,
+          pieces.getSprite(4 - j, i),
+          dx,
+          dy,
+          world: world,
+        ));
+      }
+    }
   }
 
   @override
