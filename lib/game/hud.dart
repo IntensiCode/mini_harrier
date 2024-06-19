45f32e564eb69fa13ee8a4d4f77f83552a6dcf10
Daniel Lukic <daniel.lukic@gmx.net>
2024-06-19 22:24:31 +0200

    Rework hud

--- lib/game/hud.dart
+++ lib/game/hud.dart
@@ -1,12 +1,13 @@
-import 'dart:ui';
-
 import 'package:flame/components.dart';
 import 'package:flame/sprite.dart';
+import 'package:flutter/material.dart';
 import 'package:mini_harrier/core/common.dart';
+import 'package:mini_harrier/util/bitmap_text.dart';
 
 import '../core/soundboard.dart';
 import '../scripting/game_script_functions.dart';
 import '../util/auto_dispose.dart';
+import '../util/fonts.dart';
 import 'captain.dart';
 
 class Hud extends PositionComponent with AutoDispose, GameScriptFunctions {
@@ -18,52 +19,74 @@ class Hud extends PositionComponent with AutoDispose, GameScriptFunctions {
 
   final addPos = Vector2(xCenter, 20);
 
+  double value() => player.life * 100 / player.maxLife;
+
   @override
   void onLoad() async {
     priority = 100;
-    energy = sheet(await image('hud_meter.png'), 1, 9);
-    add(VShmupHudMeter(energy, () => player.life * 100 / player.maxLife)
-      ..position.setValues(xCenter, 10)
-      ..anchor = Anchor.center);
+    add(VShmupHudMeter(value));
+    fontSelect(menuFont, scale: 0.25);
+    _suit = textXY('Suit Integrity', xCenter, 19, anchor: Anchor.center);
+  }
+
+  late BitmapText _suit;
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (value() == 0 && _suit.text == 'Suit Integrity') {
+      _suit.removeFromParent();
+      _suit = textXY('Suit Destroyed', xCenter, 19, anchor: Anchor.center);
+    }
   }
 }
 
-class VShmupHudMeter extends SpriteComponent with HasVisibility {
-  VShmupHudMeter(this.sheet, this.value);
+class VShmupHudMeter extends Component with HasPaint, HasVisibility {
+  VShmupHudMeter(this.value);
 
-  final SpriteSheet sheet;
   final double Function() value;
 
-  @override
-  void onLoad() => _update();
-
-  void _update() {
-    final row = ((100 - value()) / 100 * sheet.rows).clamp(0, sheet.rows - 1).toInt();
-    sprite = sheet.getSprite(row, 0);
-  }
-
   double blinkTime = 0;
-  double warnTime = 0;
+  double warnTime = 2;
 
   @override
   void update(double dt) {
     blinkTime += dt;
     if (blinkTime > 1) blinkTime -= 1;
-    isVisible = value() > 0 && (value() > 22 || blinkTime > 0.25 ? true : false);
-    if (value() == 0) return;
+    isVisible = (value() > 40 || blinkTime > 0.25 ? true : false);
 
-    if (!isVisible) {
+    var v = value();
+
+    if (v > 0 && v <= 40) {
       warnTime += dt;
-      if (warnTime > 1) {
-        warnTime -= 1;
+      if (warnTime > 2) {
+        warnTime -= 2;
         soundboard.play(Sound.warning, volume: 0.3);
       }
+    }
+
+    // set paint color to reg, orange, yellow, green based on value():
+    paint.color = switch (v) {
+      > 80 => Colors.green,
+      > 60 => Colors.yellow,
+      > 40 => Colors.orange,
+      _ => Colors.red,
+    };
+
+    if (v == 0) v = 100;
+    if (v != currentRect.width) {
+      currentRect = RRect.fromLTRBR(xCenter - 50, 5, xCenter - 50 + value(), 15, const Radius.circular(5));
     }
   }
 
   @override
   void render(Canvas canvas) {
-    _update();
     super.render(canvas);
+    canvas.drawRRect(baseRect, basePaint);
+    canvas.drawRRect(currentRect, paint);
   }
+
+  var currentRect = RRect.fromLTRBR(xCenter - 50, 5, xCenter + 50, 15, const Radius.circular(5));
+  final baseRect = RRect.fromLTRBR(xCenter - 51, 4, xCenter + 51, 16, const Radius.circular(5));
+  final basePaint = Paint()..color = const Color(0x80000000);
 }
